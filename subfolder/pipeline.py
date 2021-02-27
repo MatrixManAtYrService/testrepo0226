28bdeec04e0475a4bbbca476831b1a20dc42bd41
Matt Rixman <MatrixManAtYrService@users.noreply.github.com>
2021-02-26 17:09:45 -0700

    and another one

--- subfolder/pipeline.py
+++ subfolder/pipeline.py
@@ -4,19 +4,8 @@ import conducto as co
 def main() -> co.Serial:
     with co.Serial() as node:
 
-        A = co.Image(dockerfile="Dockerfile", copy_repo=True)
-        node["A context"] = co.Exec("ls /context", image=A)
-        # - subfolder
-        # - this_is_repo_root
-
         B = co.Image(dockerfile="Dockerfile", context=".", copy_repo=True)
         node["B context"] = co.Exec("ls /context", image=B)
-        # - Dockerfile
-        # - pipeline.py
-        # - this_folder_containes_dockerfile
-
-        C = co.Image(dockerfile="Dockerfile", context=".")
-        node["C context"] = co.Exec("ls /context", image=C)
         # - Dockerfile
         # - pipeline.py
         # - this_folder_containes_dockerfile
